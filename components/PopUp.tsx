7c049fe700e578ae6eb26224dc83cb85f996d931
Nitin Rana <nitinranagraphian@gmail.com>
2022-12-08 22:50:28 +0530

    donee

---
+++ components/PopUp.tsx
@@ -0,0 +1,63 @@
+// import styles from "../styles/Home.module.scss";
+
+// export default function PopUp(){
+//     return(
+//         <main className={styles.popUp}> 
+        
+//         </main>
+//     )
+// }
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function PopUp() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>"Future Contact US Page"</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti facere perferendis autem quibusdam ea odio numquam pariatur aut reprehenderit, nulla, a et nesciunt corrupti veritatis officiis, placeat omnis. Corrupti, possimus.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import Bike from "../components/Bike";
 import ChooseEV from "../components/chooseEV";
 import LandingFace from "../components/landingFace";
+import PopUp from "../components/PopUp";
 import Scooter from "../components/Scooter";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  useEffect(() => {
+    setTimeout(()=> setShowPopUp(true), 5000);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +23,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        {showPopUp && <PopUp/>}
         <LandingFace />
         <ChooseEV />
         <div id="Scooter">
